68c6e5e4b05e34ebe94cc5352769f25888b9dfac
Tomás Palma <tresillo2017@gmail.com>
2021-12-20 14:15:31 +0100

    1

---
+++ TomasPython01.py
@@ -0,0 +1,33 @@
+a = int(input("Escribe el primer numero "))
+b = int(input("Escribe el segundo numero "))
+
+
+def os():
+    print (a + b) 
+    print (a - b)
+    print (a * b)
+    print (a / b)
+    print (a // b)
+    print (a % b)
+
+
+# def texto():
+#    print (suma + "= ")
+#    print (resta)
+#    print (multiplicacion)
+#    print (division)
+#    print (division_entera)
+#    print (resto_division)
+def resultado():
+    print("Operaciones disponibles con " + a + " " + b)
+
+str(resultado)
+os()
+
+
+
+
+
+
+
+
