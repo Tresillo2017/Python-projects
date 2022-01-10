de3f26e794c964e5bda3f299e807fe7d5f519832
Tresillocrack <tresillo2017@gmail.com>
2022-01-10 19:13:06 +0100

    03 final update

--- Codigos/TomasPython03.py
+++ Codigos/TomasPython03.py
@@ -14,10 +14,17 @@ print("Escribe un numero que sea par", bcolors.OKBLUE)
 numero = input()
 print(bcolors.ENDC)
 resultado = int(numero) % 2
-if numero == 0:
-    print(bcolors.OKBLUE, " CORRECTO !!!", bcolors.ENDC)
+# print(resultado) Debug only don't uncoment this unless you know what are you doing
+if resultado == 0:
+    print(bcolors.OKBLUE,"CORRECTO !!!", bcolors.ENDC)
+    print(bcolors.HEADER,"El numero", numero, "es un numero par", bcolors.ENDC)
 
-print(bcolors.HEADER, "El numero", numero, " ", "es un numero par", bcolors.ENDC)
+elif resultado == 1:
+    print(bcolors.OKBLUE,"INCORRECTO !!!", bcolors.ENDC)
+    print(bcolors.HEADER,"El numero", numero,"es un numero",bcolors.WARNING,"impar", bcolors.ENDC)
 
-print(int(numero), "/ ", "2", "= ", numero / 2)
+else:
+    print(bcolors.FAIL,"Lo siento el numero ",numero, "no puedo clasificarlo", bcolors.ENDC)
+
+print(int(numero), "/ ", "2", "= ", int(numero) / 2)
 
