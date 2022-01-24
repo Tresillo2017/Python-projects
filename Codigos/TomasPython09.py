f2071c049c8198c5ee0711db324f311856d6d4cc
Tomás Palma <tresillo2017@gmail.com>
2022-01-24 13:58:56 +0100

    Python09

---
+++ Codigos/TomasPython09.py
@@ -0,0 +1,20 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print("Programa que calcula todos los numeros primos desde 2 hasta el numero dado")
+print("Escribe un numero")
+n = eval(input(bcolors.OKBLUE))
+numero = n-1
+print(bcolors.ENDC)
+
+for p in range(2, numero):
+    if (n % p == 0):
+        print(bcolors.OKCYAN, p, bcolors.ENDC, "es divisor de", bcolors.HEADER, n, bcolors.ENDC)
